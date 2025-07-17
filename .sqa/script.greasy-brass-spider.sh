fe22f71858bdfcf63649264ece1ce41f0cd2dfb6
Pablo Orviz <orviz@ifca.unican.es>
2025-07-17 12:00:17 +0200

    Add JePL file structure

---
+++ .sqa/script.greasy-brass-spider.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/NicoSchlw/run_dynamic_source_inversion_FD3D_TSN/DTC74/WF7401/ST740102/src &&
+    mdl --json  .
+)
